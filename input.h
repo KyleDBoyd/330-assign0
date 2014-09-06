609e5a02376250251297da5a9f060c4c4c4f6082
Kyle Douglas Boyd <boydkd@stumail.csci.viu.ca>
2014-09-06 02:15:21 -0700

    Initial Commit

---
+++ input.h
@@ -0,0 +1,15 @@
+#infdef INPUT_H
+#define INPUT_H
+
+class Input {
+    public:
+        // Constructor
+        Input();
+        // Destructor
+        ~Input();
+    private:
+        boolean checkName(string s);
+        boolean checkIncome(sting s);
+         
+    
+}
