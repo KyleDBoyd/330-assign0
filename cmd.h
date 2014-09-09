b44ddbba088d5a3212dcee55bf1fc3c842568100
KyleDBoyd <kyleboyd@live.ca>
2014-09-09 16:34:07 -0700

    Update cmd.h

--- cmd.h
+++ cmd.h
@@ -5,12 +5,16 @@ using namespace std;
 
 class Cmd {
 
+    fileName;
+    
     public:
 
         // Constructor
         Cmd(int argc, char *argv[]);
         // Destructor
         ~Cmd();
+        // Get file name from command line argument
+        getFileName();
 
 };
 
